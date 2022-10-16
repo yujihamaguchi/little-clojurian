3871ab78a1de1a929158dacec784447b83edf850
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2022-10-17 08:14:39 +0900

    tweak

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1510,15 +1510,17 @@
   (apply map list (partition n coll)))
 
 ;; Q109: Write a function which can rotate a sequence in either direction.(p44)
-;; (= (__ 2 [1 2 3 4 5]) '(3 4 5 1 2))
-;; (= (__ -2 [1 2 3 4 5]) '(4 5 1 2 3))
-;; (= (__ 6 [1 2 3 4 5]) '(2 3 4 5 1))
-;; (= (__ 1 '(:a :b :c)) '(:b :c :a))
-;; (= (__ -4 '(:a :b :c)) '(:c :a :b))
+;;       (= (__ 2 [1 2 3 4 5]) '(3 4 5 1 2))
+;;       (= (__ -2 [1 2 3 4 5]) '(4 5 1 2 3))
+;;       (= (__ 6 [1 2 3 4 5]) '(2 3 4 5 1))
+;;       (= (__ 1 '(:a :b :c)) '(:b :c :a))
+;;       (= (__ -4 '(:a :b :c)) '(:c :a :b))
+;;       Write using `take and drop` version and `recursion` one.
+;; take and drop
 (defn p44 [n coll]
   (let [n' (mod n (count coll))]
     (concat (drop n' coll) (take n' coll))))
-;; my naive answer
+;; recursion
 ;; (defn p44 [n coll]
 ;;   (cond
 ;;     (= n 0) coll
