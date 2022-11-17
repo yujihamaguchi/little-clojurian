84bdd062fa2f3bbd37c0f02b1a6d8a751d6fbc0a
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2022-11-18 08:18:54 +0900

    tweak

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1654,8 +1654,8 @@
 ;; Q116: Take a set of functions and return a new function that takes a variable number of arguments
 ;;       and returns a sequence containing the result of applying
 ;;       each function left-to-right to the argument list.(p59)
-;; Special Restrictions
-;; juxt
+;;       [ Special Restrictions ]
+;;       - juxt
 ;; (= [21 6 1] ((__ + max min) 2 3 5 1 6 4))
 ;; (= ["HELLO" 5] ((__ #(.toUpperCase %) count) "hello"))
 ;; (= [2 6 4] ((__ :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
