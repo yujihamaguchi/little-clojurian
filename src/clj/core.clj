20f50eaf94f25ef0435d802b8bdbb0155bf0004b
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2022-12-22 08:19:21 +0900

    tewak

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -1802,7 +1802,6 @@
             (= #{1} (set/intersection (set (my-factors n)) (set (my-factors m)))))]
     (count (filter (partial coprime? n) (range 1 (inc n))))))
 
-
 ;; Q122: Write a function which finds all the anagrams in a vector of words.
 ;;       A word x is an anagram of word y if all the letters in x can be rearranged in a different order to form y.
 ;;       Your function should return a set of sets, where each sub-set is a group of words which are anagrams of each other.
