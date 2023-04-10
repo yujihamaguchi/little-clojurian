3fd193f5b8e697998d80f84ed1b2c5547e977834
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2023-04-11 07:32:50 +0900

    tweak

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -2294,12 +2294,12 @@
 
 ;; Q142: Write a function which flattens any nested combination of sequential things (lists, vectors, etc.),
 ;;       but maintains the lowest level sequential items. The result should be a sequence of sequences with only one level of nesting.(p93)
+;; (= (__ '((1 2)((3 4)((((5 6)))))))
+;;    '((1 2)(3 4)(5 6)))
 ;; (= (__ [["Do"] ["Nothing"]])
 ;;    [["Do"] ["Nothing"]])
 ;; (= (__ [[[[:a :b]]] [[:c :d]] [:e :f]])
 ;;    [[:a :b] [:c :d] [:e :f]])
-;; (= (__ '((1 2)((3 4)((((5 6)))))))
-;;    '((1 2)(3 4)(5 6)))
 (defn p93
   [coll]
   (when (seq coll)
