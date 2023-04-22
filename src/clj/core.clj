243e2382ea74334bb961c739e64f522430e81f07
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2023-04-23 08:23:05 +0900

    tweak

--- src/clj/core.clj
+++ src/clj/core.clj
@@ -2355,15 +2355,15 @@
 ;;       Write a function which accepts an integer n, a predicate p, and a sequence.
 ;;       It should return a lazy sequence of items in the list up to, but not including, the nth item that satisfies the predicate.(p114)
 ;;
-;; (= [2 3 5 7 11 13]
-;;    (__ 4 #(= 2 (mod % 3))
-;;          [2 3 5 7 11 13 17 19 23]))
-;; (= ["this" "is" "a" "sentence"]
-;;    (__ 3 #(some #{\i} %)
-;;          ["this" "is" "a" "sentence" "i" "wrote"]))
 ;; (= ["this" "is"]
 ;;    (__ 1 #{"a"}
 ;;          ["this" "is" "a" "sentence" "i" "wrote"]))
+;; (= ["this" "is" "a" "sentence"]
+;;    (__ 3 #(some #{\i} %)
+;;          ["this" "is" "a" "sentence" "i" "wrote"]))
+;; (= [2 3 5 7 11 13]
+;;    (__ 4 #(= 2 (mod % 3))
+;;          [2 3 5 7 11 13 17 19 23]))
 (defn p114 [n p coll]
   (letfn [(_ [acc n coll]
             (if (zero? n)
